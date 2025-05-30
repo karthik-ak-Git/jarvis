f7547cf3312a93f8232a93c9985b6bee126cce52
karthik <karthik986340@gmail.com>
2025-05-30 14:59:17 +0530

    take command and jarvis speak

--- engine/command.py
+++ engine/command.py
@@ -1,4 +1,5 @@
 import pyttsx3
+import speech_recognition as sr
 
 
 def speak(text):
@@ -10,4 +11,26 @@ def speak(text):
     engine.runAndWait()
 
 
-speak("Hello Jarvis")
+def takeCommand():
+
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("listing...")
+        r.pause_threshold = 1
+        r.adjust_for_ambient_noise(source)
+
+        audio = r.listen(source, 10, 6)
+
+    try:
+        print("Recognizing...")
+        query = r.recognize_google(audio, language='en-in')
+        print(f'user said: {query}')
+    except Exception as e:
+        return ""
+    return query.lower()
+
+
+text = takeCommand()
+
+
+speak(text)
